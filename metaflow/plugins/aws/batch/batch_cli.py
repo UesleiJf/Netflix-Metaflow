f381589674a6e2df85f7f3e42d33eb7ac21f4cb8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 02:41:27 +0000

    Adding tarfile member sanitization to extractall()

--- metaflow/plugins/aws/batch/batch_cli.py
+++ metaflow/plugins/aws/batch/batch_cli.py
@@ -79,7 +79,26 @@ def _sync_metadata(echo, metadata, datastore_root, attempt):
                 with open(tar_file_path, 'wb') as f:
                     s3.download_fileobj(bucket, key, f)
                 with tarfile.open(tar_file_path, 'r:gz') as tar:
-                    tar.extractall(td)
+                    def is_within_directory(directory, target):
+                    	
+                    	abs_directory = os.path.abspath(directory)
+                    	abs_target = os.path.abspath(target)
+                    
+                    	prefix = os.path.commonprefix([abs_directory, abs_target])
+                    	
+                    	return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                    	for member in tar.getmembers():
+                    		member_path = os.path.join(path, member.name)
+                    		if not is_within_directory(path, member_path):
+                    			raise Exception("Attempted Path Traversal in Tar File")
+                    
+                    	tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    	
+                    
+                    safe_extract(tar, td)
                 copy_tree(
                     os.path.join(td, DATASTORE_LOCAL_DIR),
                     LocalDataStore.get_datastore_root_from_config(echo_none),
